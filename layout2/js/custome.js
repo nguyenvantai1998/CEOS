500b43de4cf968b93cd1d1e6521cc365f81f7c53
NguyenVanTai <vantain124@gmail.com>
2019-08-05 17:20:36 +0700

    full index

--- layout2/js/custome.js
+++ layout2/js/custome.js
@@ -1,18 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // animation
-    // AOS.init();
-    $('.fadeOut').owlCarousel({
+  // animation
+  // AOS.init();
+  $('.fadeOut').owlCarousel({
+    items: 1,
+    animateOut: 'fadeOut',
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    // autoplayTimeout:2000
+  });
+
+  // number beautiful
+  $(".numberCounter").counterUp({ delay: 10, time: 1000 });
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
         items: 1,
-        animateOut: 'fadeOut',
-        loop: true,
-        margin: 10,
-        autoplay:true,
-        // autoplayTimeout:2000
-      });
+        nav: true
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+        margin: 20
+      }
+    }
+  })
 
-      // number beautiful
-    $(".numberCounter").counterUp({delay:10,time:1000});
+  $(".fancybox-button").fancybox({
+    prevEffect: 'none',
+    nextEffect: 'none',
+    closeBtn: false,
+    helpers: {
+      title: { type: 'inside' },
+      buttons: {}
+    }
+  });
 
-    
 })
