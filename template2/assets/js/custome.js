e2df8488218d69355617011157e6de6e0f39450f
NguyenVanTai <vantain124@gmail.com>
2019-08-13 22:47:38 +0700

    res 600

--- template2/assets/js/custome.js
+++ template2/assets/js/custome.js
@@ -1,13 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // animation
-    // AOS.init();
-    $('.fadeOut').owlCarousel({
-        items: 1,
-        loop: true,
-        margin: 10,
-        nav:true
-      });
+  // animation
+  // AOS.init();
+  $('.fadeOut').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true
+  });
 
-    
+  paypals.minicarts.render(); //use only unique class names other than paypals.minicarts.Also Replace same class name in css and minicart.min.js
+  paypals.minicarts.cart.on('checkout', function (evt) {
+    var items = this.items(),
+      len = items.length,
+      total = 0,
+      i;
+
+    // Count the number of each item in the cart
+    for (i = 0; i < len; i++) {
+      total += items[i].get('quantity');
+    }
+
+    if (total < 3) {
+      alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
+      evt.preventDefault();
+    }
+  });
+
+  // toggle nav mobile
+  $('#btn-toggle').click(function () {
+    $('.hd-nav .menu-primary').toggleClass('menu-primary-active', 500);
+  });
+
+
 })
