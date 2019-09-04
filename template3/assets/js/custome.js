00e1620b39e3573781024c800e1613ec720ceffc
NguyenVanTai <vantain124@gmail.com>
2019-09-04 16:44:16 +0700

    done promotion

--- template3/assets/js/custome.js
+++ template3/assets/js/custome.js
@@ -12,28 +12,6 @@ $(document).ready(function () {
     autoplayHoverPause: true
   });
 
-  // $('.owl-carousel').owlCarousel({
-  //   loop: true,
-  //   margin: 10,
-  //   responsiveClass: true,
-  //   responsive: {
-  //     0: {
-  //       items: 1,
-  //       nav: true
-  //     },
-  //     600: {
-  //       items: 3,
-  //       nav: false
-  //     },
-  //     1000: {
-  //       items: 5,
-  //       nav: true,
-  //       loop: false,
-  //       margin: 20
-  //     }
-  //   }
-  // })
-
   $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
@@ -100,14 +78,24 @@ $(document).ready(function () {
     }
   })
 
-  // event show nav mobile
-  $('#btn-leftmenu').click(function(){
-    $('.hd-nav-mb').addClass('hd-nav-mb-active');
-  })
-  $('#close-left').click(function(){
-    $('.hd-nav-mb').removeClass('hd-nav-mb-active');
+  // event show scroll page promotion
+  var h = $('.hd-nav').height()+100;
+  $(window).bind('scroll', function () {
+    var pos = window.scrollY;
+    if (pos >= h) {
+      $('.promotionMenuIcon').addClass('promotionMenuIcon-active');
+    } else {
+      $('.promotionMenuIcon').removeClass('promotionMenuIcon-active');
+    }
   })
 
+  // event show nav mobile
+  $('#btn-leftmenu').click(function () {
+    $('.hd-nav-mb').addClass('hd-nav-mb-active');
+  })
+  $('#close-left').click(function () {
+    $('.hd-nav-mb').removeClass('hd-nav-mb-active');
+  })
 
   // scroll to top
   var h = $('#header').height();
@@ -122,6 +110,102 @@ $(document).ready(function () {
   $('#scrollToTop').click(function () {
     $('html,body').animate({ scrollTop: 0 }, 1000);
   });
+
+  // ------------------- page ---------------------- //
+  var bigimage = $("#big");
+  var thumbs = $("#thumbs");
+  //var totalslides = 10;
+  var syncedSecondary = true;
+
+  bigimage
+    .owlCarousel({
+      items: 1,
+      slideSpeed: 2000,
+      nav: true,
+      autoplay: true,
+      dots: false,
+      loop: true,
+      responsiveRefreshRate: 200,
+      navText: [
+        '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+      ]
+    })
+    .on("changed.owl.carousel", syncPosition);
+
+  thumbs
+    .on("initialized.owl.carousel", function () {
+      thumbs
+        .find(".owl-item")
+        .eq(0)
+        .addClass("current");
+    })
+    .owlCarousel({
+      items: 4,
+      dots: true,
+      nav: true,
+      navText: [
+        '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+      ],
+      smartSpeed: 200,
+      slideSpeed: 500,
+      slideBy: 4,
+      responsiveRefreshRate: 100
+    })
+    .on("changed.owl.carousel", syncPosition2);
+
+  function syncPosition(el) {
+    //if loop is set to false, then you have to uncomment the next line
+    //var current = el.item.index;
+
+    //to disable loop, comment this block
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+    if (current < 0) {
+      current = count;
+    }
+    if (current > count) {
+      current = 0;
+    }
+    //to this
+    thumbs
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = thumbs.find(".owl-item.active").length - 1;
+    var start = thumbs
+      .find(".owl-item.active")
+      .first()
+      .index();
+    var end = thumbs
+      .find(".owl-item.active")
+      .last()
+      .index();
+
+    if (current > end) {
+      thumbs.data("owl.carousel").to(current, 100, true);
+    }
+    if (current < start) {
+      thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+    }
+  }
+
+  function syncPosition2(el) {
+    if (syncedSecondary) {
+      var number = el.item.index;
+      bigimage.data("owl.carousel").to(number, 100, true);
+    }
+  }
+
+  thumbs.on("click", ".owl-item", function (e) {
+    e.preventDefault();
+    var number = $(this).index();
+    bigimage.data("owl.carousel").to(number, 300, true);
+  });
+
 
 
 })
