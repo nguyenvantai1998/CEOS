481947403b5cc7f7a9f343349f57632ac5826aaf
Nguyễn Văn Tài <vantain124@gmail.com>
2019-10-01 19:56:20 +0700

    update

--- template4/assets/js/custome.js
+++ template4/assets/js/custome.js
@@ -17,6 +17,23 @@ $(document).ready(function () {
         }
     })
 
+    // scroll sidebar page news
+    var heightStickyNav = $('#hd-bottom-sticky').height() + 1;
+    var heightcolLeftSictky = $('#colLeftSictky').height() / 2;
+    $(window).bind('scroll', function () {
+        var posNav = window.scrollY;
+        if (posNav >= sumHeight) {
+            $('.boxStickyTop').addClass('boxStickyTop-active');
+            $('.boxStickyTop').css({ "top": heightStickyNav });
+            if (posNav > heightcolLeftSictky) {
+                $('.boxStickyTop').css({ "margin-bottom": '30%' });
+            }
+        } else {
+            $('.boxStickyTop').removeClass('boxStickyTop-active');
+            $('.boxStickyTop').css({ "top": 'unset' });
+        }
+    })
+
     // big banner home
     $('.bigBannerHome').owlCarousel({
         items: 1,
@@ -49,6 +66,14 @@ $(document).ready(function () {
                 margin: 20
             }
         }
+    });
+
+    // silde single category
+    $('.sildeSingleCategory').owlCarousel({
+        items: 1,
+        margin: 30,
+        stagePadding: 30,
+        smartSpeed: 450
     });
 
     // scroll to top
@@ -137,4 +162,62 @@ $(document).ready(function () {
     $("#amount").val("$" + $("#slider-range").slider("values", 0) +
         " - $" + $("#slider-range").slider("values", 1));
 
-})
+        $("#zoom_04").ezPlus();
+});
+
+
+
+
+// slide product single category
+$(document).ready(function () {
+    // reference for main items
+    var slider = $('#slider');
+    // reference for thumbnail items
+    var thumbnailSlider = $('#thumbnailSlider');
+    //transition time in ms
+    var duration = 500;
+
+    // carousel function for main slider
+    slider.owlCarousel({
+        loop: false,
+        nav: true,
+        items: 1
+    }).on('changed.owl.carousel', function (e) {
+        //On change of main item to trigger thumbnail item
+        thumbnailSlider.trigger('to.owl.carousel', [e.item.index, duration, true]);
+    });
+
+    // carousel function for thumbnail slider
+    thumbnailSlider.owlCarousel({
+        loop: false,
+        center: false, //to display the thumbnail item in center
+        nav: false,
+        responsive: {
+            0: {
+                items: 3
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 6
+            }
+        }
+    }).on('click', '.owl-item', function () {
+        // On click of thumbnail items to trigger same main item
+        slider.trigger('to.owl.carousel', [$(this).index(), duration, true]);
+
+    }).on('changed.owl.carousel', function (e) {
+        // On change of thumbnail item to trigger main item
+        slider.trigger('to.owl.carousel', [e.item.index, duration, true]);
+    });
+
+
+    //These two are navigation for main items
+    $('.slider-right').click(function () {
+        slider.trigger('next.owl.carousel');
+    });
+    $('.slider-left').click(function () {
+        slider.trigger('prev.owl.carousel');
+    });
+});
